6157a892ba1bb43c892feb93e919aa1da411ffe0
Logeshwaran2003 <93189506+Logeshwaran2003@users.noreply.github.com>
2021-12-19 18:01:54 +0530

    Update FOOD2.C

--- FOOD2.C
+++ FOOD2.C
@@ -3,7 +3,7 @@
 void main()
 {
    int tpoori,tpoori2,kumaran,lokeshbabu,sharuk;
-   int reat,seat,keat;
+   int keat,leat,seat;
    clrscr();
    printf("Total Poori are:\n");
    scanf("%d",&tpoori);
@@ -11,17 +11,17 @@ void main()
    scanf("%d",&kumaran);
    printf("\nenter lokeshbabu's poori percentage:\n");
    scanf("%d",&lokeshbabu);
-   reat=tpoori* kumaran/100;
-   seat=tpoori* lokeshbabu/100;
-   tpoori2=tpoori-reat-seat;
-   printf("kumaran's poori:%d\n",reat);
-   printf("lokeshbabu's poori:%d\n",seat);
+   keat=tpoori* kumaran/100;
+   leat=tpoori* lokeshbabu/100;
+   tpoori2=tpoori-keat-leat;
+   printf("kumaran's poori:%d\n",keat);
+   printf("lokeshbabu's poori:%d\n",leat);
    printf("balanced poori after kunaran and lokeshbabu ate:%d\n",tpoori2);
    printf("\nenter sharuk's poori percentage:\n");
    scanf("%d",&sharuk);
-   keat=tpoori* sharuk/100;
+   seat=tpoori* sharuk/100;
    tpoori=tpoori-keat;
-   printf("sharuk's poori:%d\n",keat);
+   printf("sharuk's poori:%d\n",seat);
    printf("totalbalance after three members ate: %d\n",tpoori);
    getch();
    }
