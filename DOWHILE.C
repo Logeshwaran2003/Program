ad12444661eecb57b08f41988332f121b062384a
Logeshwaran2003 <93189506+Logeshwaran2003@users.noreply.github.com>
2021-12-19 18:08:56 +0530

    Update DOWHILE.C

--- DOWHILE.C
+++ DOWHILE.C
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <conio.h>
-int ragul=0;
+int kumaran=0;
 void main()
 {
 do
 {
 clrscr();
    logesh:
-   printf("ragul's current status \n");
-   scanf("%d%",&ragul);
-   if(ragul==0)
+   printf("kumaran's current status \n");
+   scanf("%d%",&kumaran);
+   if(kumaran==0)
    {
-      printf("ragul is sitting \n");
-      //ragul=1;
+      printf("kumaran is sitting \n");
+      //kumaran=1;
       //getch();
       //continue;
       goto logesh;
    }
-   else  if(ragul==1)
+   else  if(kumaran==1)
    {
-   printf("ragul is standing \n");
+   printf("kumaran is standing \n");
    //getch();
    //break;
    }
    else
    {
-   printf("ragul is sleeping \n");
+   printf("kumaran is sleeping \n");
    }
    getch();
    }
-   while(ragul);
+   while(kumaran);
 }
